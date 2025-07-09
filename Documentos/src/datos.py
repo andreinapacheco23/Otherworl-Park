687913ad1817aea2df3e5b36d006371b9de16b11
andreinapacheco23 <andreina.pacheco@udea.edu.co>
2025-07-09 00:58:52 -0500

    Create datos.py

---
+++ Documentos/src/datos.py
@@ -0,0 +1,15 @@
+import datetime
+
+NOMBRE_PARQUEADERO = "Otherworld Park"
+ESPACIOS_TOTALES = 64
+TARIFA_POR_HORA = 7000
+TARIFA_POR_CUARTO_HORA = 1500
+
+ADMIN_USUARIOS = {"admin": "admin123",  "comandante": "space2025", "supervisor": "hangar456"}
+
+usuarios = {}
+naves_en_hangar = {}
+historial_naves_retiradas = []
+total_recaudado = 0
+tiempo_total_estancia = 0
+cantidad_naves_retiradas = 0
